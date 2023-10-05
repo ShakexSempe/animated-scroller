09b7d4d8859d68dc0563579e44d11968404900ec
Ray-Shakex <bshakex@gmail.com>
2023-10-05 10:35:31 +0200

    duplicate inner scroll items into the scroll element

--- js/main.js
+++ js/main.js
@@ -6,8 +6,17 @@ if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 
 function addAnimation() {
     scrollers.forEach((scroller) => {
-        scroller.setAttribute("data-animated", true)
-        console.log(scroller);
+        scroller.setAttribute("data-animated", true);
 
+        const scrollerInner = scroller.querySelector('.scroller__inner');
+        const scrollerContent = Array.from(scrollerInner.children);
+        console.log(scrollerContent);
+
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute('aria-hidden', true);
+            console.log(duplicatedItem);
+            scrollerInner.appendChild(duplicatedItem);
+        })
     });
 }
